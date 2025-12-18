206b34bdaf83b7a6c53ce8894bdce7b135afb26a
Mike <e2262669@cmaisonneuve.qc.ca>
2025-12-18 14:09:46 -0500

    first commit

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQzSddh44Do-YwE_erM_T0BciQ4Zm5y0U",
+  authDomain: "exam-final-1a502.firebaseapp.com",
+  projectId: "exam-final-1a502",
+  storageBucket: "exam-final-1a502.firebasestorage.app",
+  messagingSenderId: "672664526332",
+  appId: "1:672664526332:web:a570fa744ed9fc9d5fe679"
+};
+
+const app = initializeApp(firebaseConfig);
